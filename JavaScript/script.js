0a6d037504caddcd5a1f4dff61f1807ddefe553b
filmboyJames <filmboynationwide@gmail.com>
2022-09-27 12:25:25 +0100

    JavaScript save 3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -89,16 +89,16 @@ console.log(string1 + result);
 let string2=`13 * 12 = ${13*12}, 1 + 1= ${1+1}`;
 console.log(string2);
 
-let a;
-let b = "12345";
-let c = 12344;
-let d = true;
+//let a;
+//let b = "12345";
+//let c = 12344;
+//let d = true;
 let e = {a:"JavaScript"};
 
-console.log(typeof(a));
-console.log(typeof(b));
-console.log(typeof(c));
-console.log(typeof(d));
+//console.log(typeof(a));
+//console.log(typeof(b));
+//console.log(typeof(c));
+//console.log(typeof(d));
 console.log(typeof(e));
 
 let totalMoney = 4000;
@@ -106,3 +106,115 @@ let moneyPaidSoFar = 2348;
 let totalLeftToPay;
 
 console.log(`%cThe total bill is £${totalMoney} and the remaining amount of money to be paid is £${totalMoney - moneyPaidSoFar}`, "font-style:italic; color:white; background-color:black");
+
+// ASI = Automatic Semi-Colon Insertion
+//JavaScript automatically inserts semi-colons for us
+
+//let a = 1;
+//let b = 1;
+
+//a
+//++
+//b
+
+//console.log(`a = ${a}\nb= ${b}`);
+
+//ASI interprets the above as a; ++b; etc
+
+//let c = 1;
+//let d = 1;
+//c++; d;
+
+//console.log(`c = ${c}\nd= ${d}`);
+
+
+//ITERATION
+//Loops
+
+//FOR
+//Uses a counter and runs while a condition is met
+
+for(let i=0; i<10; i++){
+    console.log(`i=${i}`)
+};
+
+//WHILE
+//Works with Booleans, runs until a condition is no longer true
+
+let condition=true;
+let increment=0;
+while(condition === true){
+    console.log(`increment = ${increment}`);
+    increment++;
+
+    if(increment === 3) {
+        condition = false;
+    }
+}
+
+//DO-WHILE
+//Like a while loop but it will ALWAYS run at least once
+
+let a =false;
+do{
+    console.log("Run Forrest Run");
+} while(a===true);
+
+//SWITCH CASES
+//These run until a break or return, and can be much more effective than large if-else-if statements
+
+let num =1;
+switch(num){
+    case 0:
+        console.log("number is zero");
+    case 1:
+        console.log("We are number one");
+    case 4:
+        console.log("We're at 4");
+    case 10:
+        console.log("We made it to ten!");
+        break;
+    default: 
+        console.warn("Not here");
+}
+
+//Iterator is equal to 2
+//Condition is i<=6
+//if true print i+1 
+//then iterate i by 2
+//if false stop
+//WHILE
+let i=2;
+while(i<=6){
+    console.log(i+1);
+    i = i+2;
+}
+
+//FOR
+for(let i = 2; i<=6; i = i+2){
+    console.log(i+1);
+}
+
+//Iteration Exercises 
+//1 and 4
+
+for(let a1=100; a1<=200; a1++){
+    console.log(a1);
+}
+
+let a4=100;
+while(a4<=200){
+    console.log(a4);
+    a4++;
+}
+
+//2 and 4
+
+for(let a2=100; a2<=200; a2++){
+    switch(a2%2){
+        case 0:
+            console.log("-");
+        case 1:
+            console.log("*");
+    }
+}
