d775682717d9ecde750312960be2c32408851272
filmboyJames <filmboynationwide@gmail.com>
2022-09-30 09:51:59 +0100

    Calculator Fri 30th Sept

--- JavaScript/DOM Manipulation/calc.js
+++ JavaScript/DOM Manipulation/calc.js
@@ -11,43 +11,36 @@ const output = document.getElementById("output");
 const plus = () => {
     output.value = parseInt(input1.value)+parseInt(input2.value);
     console.log(output.value);
-    history();
-
 }
 
 const minus = () => {
     output.value = parseInt(input1.value)-parseInt(input2.value);
     console.log(output.value);
-    history();
-
 }
 
 const times = () => {
     output.value = parseInt(input1.value)*parseInt(input2.value);
     console.log(output.value);
-    history();
-
 }
 
 const divide = () => {
     output.value = parseInt(input1.value)/parseInt(input2.value);
     console.log(output.value);
-    history();
-
 }
 
 const modulo = () => {
     output.value = parseInt(input1.value)%parseInt(input2.value);
     console.log(output.value);
-    history();
-
 }
 
-//ADDITIONAL EXTRA - maaaybe...
-
-const history = () => {
-    const hist = document.createElement("p");
-    hist.innerText = output.value;
-    const addToBody = document.body.appendChild(hist);const output2 = document.getElementById("output");
-    output2.prepend(addToBody);
+const numberInput = (num) => {
+    input1.value = num;
+    console.log(input1.value);
 }
+
+const numberInput2 = (num) => {
+    input2.value = num;
+    console.log(input1.value);
+}
+
+
